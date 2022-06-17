c2ccaf5dcb510d31f832e3f7f2180082a9ae92e6
Khar Dmitry <qwedsazxc8250@gmail.com>
2022-06-17 21:50:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+class Array:
+    def __init__(self, a = []):
+        self.array = a
+    def Append(self, v = None):
+        return self.array.append(v)
+    def Print(self):
+        print(self.array)
+    def QuickSort(self, left = 0, right = 0, c=0):
+        if self.array == []:
+            return self.array
+        else:
+            i = left
+            j = right
+            pivot = self.array[(i + j) // 2]
+            while i <= j:
+                while self.array[i] < pivot:
+                    c += 1
+                    i += 1
+                c += 1
+                while self.array[j] > pivot:
+                    c += 1
+                    j -= 1
+                c += 1
+                if i <= j:
+                    a = self.array[i]
+                    self.array[i] = self.array[j]
+                    self.array[j] = a
+                    i += 1
+                    j -= 1
+            if left < j:
+                self.QuickSort(left, j, c)
+            if i < right:
+                self.QuickSort(i, right, c)
+            return self.array
